f9ae6cfa2bce504ec41b4a079083211e86aa39ea
lawrence-cs-student <mark.lawrence.gonzales07@gmail.com>
2026-05-15 11:27:28 +0800

    modified a route in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -60,7 +60,7 @@ async function startServer() {
     });
     
     
-    app.use('*', (req, res) => {
+    app.use('*splat', (req, res) => {
         res.status(404).json({ message: 'Route not found' });
     });
     
